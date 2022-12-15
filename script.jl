e690e815774a05bb742b08e197f908fca1ea9988
tlienart <tlienart@me.com>
2022-12-15 09:37:46 +0100

    simpler

--- script.jl
+++ script.jl
@@ -1,8 +1,12 @@
-using NodeJS
+import Pkg
+Pkg.activate(".")
+Pkg.instantiate()
+
+import NodeJS
 
 for i in 1:50
     println("*"^i)
 end
 
-println(nodejs_cmd())
-println(npm_cmd())
+println(NodeJS.nodejs_cmd())
+println(NodeJS.npm_cmd())
